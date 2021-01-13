d91c1e3fe75d0cbc98e269fb9947a6551c6dd0dc
Azzarox <62171309+Azzarox@users.noreply.github.com>
2021-01-13 12:22:54 +0200

    comments

--- Python Advanced/1. Lists-As-Stacks-And-Queues/EXE-4.Fashion_Boutique.py
+++ Python Advanced/1. Lists-As-Stacks-And-Queues/EXE-4.Fashion_Boutique.py
@@ -1,4 +1,4 @@
-# 100/100
+# 100/100 solo done
 
 clothes_box = [int(el) for el in input().split()]
 
@@ -9,12 +9,15 @@ counter = 1
 
 while clothes_box:
     cloth = clothes_box.pop()
-    if cloth > temp_cap_rack:
+
+    if cloth > temp_cap_rack:  # check if the item can be put in the rack (isn't bigger than rack capacity)
         temp_cap_rack = capacity_rack
         counter += 1
-    if temp_cap_rack <= 0:
+
+    if temp_cap_rack == 0:   # check if the rack is equal (no more space left) to 0
         temp_cap_rack = capacity_rack
         counter += 1
+
     temp_cap_rack -= cloth
 
 print(counter)
