5f6001cdd36d4809eabd7c7a8dd6c7c39f2f32d7
Azzarox <62171309+Azzarox@users.noreply.github.com>
2021-02-16 15:16:39 +0200

    Even_Matrix

---
+++ Python Advanced/4. List-Comprehensions/LAB-3.Even_Matrix.py
@@ -0,0 +1,59 @@
+def str_int(inp_split):
+    return [int(x) for x in inp_split]
+
+
+def read_matrix():
+    rows_count = int(input())
+    return [str_int(input().split(", ")) for _ in range(rows_count)]
+
+
+def is_even(x):
+    return x % 2 == 0
+
+
+def get_even(values):
+    return [x for x in values if is_even(x)]
+
+
+def get_even_matrix(matrix):
+    return [get_even(row) for row in matrix]
+
+
+def print_result(matrix):
+    print(matrix)
+
+
+matrix = read_matrix()
+even_matrix = get_even_matrix(matrix)
+print_result(even_matrix)
+
+# TODO: от презентацията
+
+# def strs_to_ints(strs):
+#     return [int(x) for x in strs]
+#
+#
+# def read_matrix():
+#     rows_counts = int(input())
+#     return [strs_to_ints(input().split(', ')) for _ in range(rows_counts)]
+#
+#
+# def is_even(x):
+#     return x % 2 == 0
+#
+#
+# def get_even(values):
+#     return [x for x in values if is_even(x)]
+#
+#
+# def get_even_matrix(matrix):
+#     return [get_even(row) for row in matrix]
+#
+#
+# def print_result(matrix):
+#     print(matrix)
+#
+#
+# matrix = read_matrix()
+# even_matrix = get_even_matrix(matrix)
+# print_result(even_matrix)
